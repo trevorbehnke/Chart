18765f54f760a3baba227e7715c97c1cf3bfa210
Trevor Behnke <me@trevorbehnke.com>
2019-11-30 17:44:47 -0800

    ch5

--- chart_project/urls.py
+++ chart_project/urls.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
+
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Local apps
+    path('accounts/', include('users.urls')),
     path('', include('charts.urls')),
 ]
