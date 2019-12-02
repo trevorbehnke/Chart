7949ba9d34e057dfc65b800fa29e9720a3525c6b
Trevor Behnke <me@trevorbehnke.com>
2019-12-01 17:41:26 -0800

    secure_proxy_ssl_header and allowed_hosts update

--- chart_project/settings.py
+++ chart_project/settings.py
@@ -192,6 +192,7 @@ if ENVIRONMENT == 'production':
     SECURE_CONTENT_TYPE_NOSNIFF = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Heroku
 db_from_env = dj_database_url.config(conn_max_age=500)
