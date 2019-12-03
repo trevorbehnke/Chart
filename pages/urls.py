be33086862effc3450d7a8ed5a93776ae6b37cdf
Trevor Behnke <me@trevorbehnke.com>
2019-12-02 20:10:35 -0800

    chart 2

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,11 @@
 # pages/urls.py
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, ChartPageView
 
 
 urlpatterns = [
-    path('chart/', AboutPageView.as_view(), name='chart'),
+    path('chart/', ChartPageView.as_view(), name='chart'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('', HomePageView.as_view(), name='home'),
 ]
